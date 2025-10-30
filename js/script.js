3019b4d543b9546d33f9069b3f7f72bbe77417d6
Wissembrahem <wbrahem19@gmail.com>
2025-10-30 15:51:28 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+*//Mail
+//Crea un array con delle email di chi può acedere.
+//Chiedi all’utente la sua email,
+//controlla che sia nella lista di chi può accedere,
+//stampa un messaggio appropriato sull’esito del controllo.
+//Non è necessario provvedere alla validazione delle email.
+//NON USARE INCLUDES!
