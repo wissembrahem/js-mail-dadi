17e1976217f6fa49bf55661dff6cba46baa676cf
Wissembrahem <wbrahem19@gmail.com>
2025-11-01 18:55:37 +0100

    secondo esercizio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
-*//Mail
-//Crea un array con delle email di chi può acedere.
+//Mail
+// 1° Crea un array con delle email di chi può acedere.
+
 //Chiedi all’utente la sua email,
+
 //controlla che sia nella lista di chi può accedere,
+
 //stampa un messaggio appropriato sull’esito del controllo.
 //Non è necessario provvedere alla validazione delle email.
 //NON USARE INCLUDES!
+
+
+//let email = ["wbrahem@gmail.it", "alessio5@hotmail.it", "degregorio@gmail.it"]
+
+//const myEmail = prompt('inserisci la tua email')
+
+//let trovato = false
+
+//for (let i=0 ; i < email.length; i++) { // i = 0
+//    if ( email[i] === myEmail ) 
+//        trovato = true;
+//}
+//console.log(trovato); // false
+
+
+//Gioco dei dadi
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+      const myNumber = Math.floor(Math.random()*6)+1;
+      const pcNumber = Math.floor(Math.random()*6)+1;
+
+      let win = false
+      if (myNumber<pcNumber) {
+        win = true
+      }
+      console.log(myNumber)
+      console.log(pcNumber)
+      console.log(win)
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//genera un  numero da 1 a 6 per entrambi 
+//verificare chi ha il punteggio piu alto
+//se il computer ha il punteggio piu alto computer win se io ho il numero piu alto vinco io 
