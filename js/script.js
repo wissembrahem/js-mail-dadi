15ad0da9959f3f89876cc8a2e0cebaeea86df8b6
Wissembrahem <wbrahem19@gmail.com>
2025-11-03 18:16:51 +0100

    termine del eserciz

--- js/script.js
+++ js/script.js
@@ -26,17 +26,17 @@
 //Gioco dei dadi
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 
-const myNumber = Math.floor(Math.random() * 6) + 1;
-const pcNumber = Math.floor(Math.random() * 6) + 1;
-console.log(myNumber);
-console.log(pcNumber);
-if (myNumber < pcNumber) {
-    console.log("Hai perso");
-} else if (myNumber > pcNumber) {
-    console.log("Hai vinto");
-} else {
-    console.log("Pareggio");
-}
+//const myNumber = Math.floor(Math.random() * 6) + 1;
+//const pcNumber = Math.floor(Math.random() * 6) + 1;
+//console.log(myNumber);
+//console.log(pcNumber);
+//if (myNumber < pcNumber) {
+//    console.log("Hai perso");
+//} else if (myNumber > pcNumber) {
+//    console.log("Hai vinto");
+//} else {
+//    console.log("Pareggio");
+//}
 
 
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
